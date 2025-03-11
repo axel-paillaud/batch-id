805fe7d803ce1b5112bfcf6c7d09a93890a7c00e
axel-paillaud <contact@axelweb.fr>
2025-03-11 09:05:21 +0100

    first commit, init main file

---
+++ batch-id.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Batch ID
+ * Description:       Barcode batch management by customer.
+ * Version:           1.0.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            Axel Paillaud
+ * Author URI:        https://axelweb.fr/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       batch-id
+ * Domain Path:       /languages
+ */
