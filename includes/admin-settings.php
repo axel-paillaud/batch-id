5c30155da6c69d226488184ec941782865cc46c5
axel-paillaud <contact@axelweb.fr>
2025-03-11 11:46:37 +0100

    add 'customer' column, with link to customer if batch id have a customer associated

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -21,6 +21,7 @@ function batch_id_admin_page() {
     global $wpdb;
     $table_batch_ids = $wpdb->prefix . 'batch_ids';
     $table_barcodes = $wpdb->prefix . 'barcodes';
+    $table_users = $wpdb->prefix . 'users';
     $message = '';
 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['batch_id'])) {
@@ -51,7 +52,7 @@ function batch_id_admin_page() {
     }
 
     // Fetch existing Batch IDs
-    $batch_ids = $wpdb->get_results("SELECT batch_id FROM $table_batch_ids ORDER BY id DESC");
+    $batch_ids = $wpdb->get_results("SELECT batch_id, customer_id FROM $table_batch_ids ORDER BY id DESC");
 
     ?>
     <div class="wrap">
@@ -74,17 +75,28 @@ function batch_id_admin_page() {
                 <thead>
                     <tr>
                         <th><?php _e('Batch ID', 'batch-id'); ?></th>
+                        <th><?php _e('Customer', 'batch-id'); ?></th>
                         <th><?php _e('Barcodes', 'batch-id'); ?></th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php foreach ($batch_ids as $batch) :
-                        // Vérifier si tous les barcodes du batch sont utilisés
+                        // Check if all barcodes of the batch are used
                         $unused_count = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_barcodes WHERE batch_id = %s AND is_used = 0", $batch->batch_id));
                         $batch_style = ($unused_count == 0) ? 'style="text-decoration: line-through; color: red;"' : '';
+
+                        // Retrieve customer information if a customer_id is defined
+                        $customer_name = 'Non attribué';
+                        if (!is_null($batch->customer_id)) {
+                            $customer_data = get_userdata($batch->customer_id);
+                            if ($customer_data) {
+                                $customer_name = '<a href="' . esc_url(admin_url("user-edit.php?user_id=" . $batch->customer_id)) . '" target="_blank">' . esc_html($customer_data->display_name) . '</a>';
+                            }
+                        }
                     ?>
                         <tr>
                             <td <?php echo $batch_style; ?>><?php echo esc_html($batch->batch_id); ?></td>
+                            <td><?php echo $customer_name; ?></td>
                             <td>
                                 <button class="toggle-barcodes button" data-batch="<?php echo esc_attr($batch->batch_id); ?>">
                                     <?php _e('Voir les barcodes', 'batch-id'); ?>
