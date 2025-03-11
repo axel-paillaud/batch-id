2c2ae63ddcc61d2390095f89dbafa0d1120c36e1
axel-paillaud <contact@axelweb.fr>
2025-03-11 13:07:53 +0100

    add script to handle delete batch ID

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -15,27 +15,51 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-// Autocomplete for customer field
 jQuery(document).ready(function($) {
-    $("#customer").autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: ajaxurl, // URL WP's admin-ajax.php
-                dataType: "json",
-                data: {
-                    action: "batch_id_search_customers",
-                    term: request.term
-                },
-                success: function(data) {
-                    response(data);
-                }
-            });
+  // Autocomplete for customer field
+  $("#customer").autocomplete({
+    source: function(request, response) {
+      $.ajax({
+        url: ajaxurl, // URL WP's admin-ajax.php
+        dataType: "json",
+        data: {
+          action: "batch_id_search_customers",
+          term: request.term
         },
-        minLength: 2,
-        select: function(event, ui) {
-            $("#customer").val(ui.item.label);
-            $("#customer_id").val(ui.item.value);
-            return false;
+        success: function(data) {
+          response(data);
         }
-    });
+      });
+    },
+    minLength: 2,
+    select: function(event, ui) {
+      $("#customer").val(ui.item.label);
+      $("#customer_id").val(ui.item.value);
+      return false;
+    }
+  });
+
+  // Handle delete batch ID
+  $(".delete-batch").click(function() {
+    let batchId = $(this).data("batch");
+    let row = $(this).closest("tr");
+
+    if (confirm("Voulez-vous vraiment supprimer ce Batch ID et tous ses barcodes ?")) {
+      $.ajax({
+        url: ajaxurl,
+        type: "POST",
+        data: {
+          action: "batch_id_delete",
+          batch_id: batchId
+        },
+        success: function(response) {
+          if (response.success) {
+            row.fadeOut("fast", function() { $(this).remove(); });
+          } else {
+            alert("Erreur lors de la suppression : " + response.data);
+          }
+        }
+      });
+    }
+  });
 });
