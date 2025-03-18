d7c7bd260ee6089b22e491f72e55dbffb6274b1e
axel-paillaud <contact@axelweb.fr>
2025-03-18 15:10:13 +0100

    update sql table. create batch_types table. add type_id column on batch_ids table

--- includes/db-install.php
+++ includes/db-install.php
@@ -8,11 +8,21 @@ function batch_id_create_tables() {
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
 
+    // Batch Types table
+    $table_batch_types = $wpdb->prefix . 'batch_types';
+    $sql_batch_types = "CREATE TABLE $table_batch_types (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        description TEXT DEFAULT NULL,
+        prefix INT NOT NULL UNIQUE
+    ) $charset_collate;";
+
     // Batch ID table
     $table_batch_ids = $wpdb->prefix . 'batch_ids';
-    $sql1 = "CREATE TABLE $table_batch_ids (
+    $sql_batch_ids = "CREATE TABLE $table_batch_ids (
         id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
         batch_id VARCHAR(20) NOT NULL UNIQUE,
+        type_id INT NOT NULL,
         customer_id BIGINT(20) UNSIGNED DEFAULT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -23,7 +33,7 @@ function batch_id_create_tables() {
 
     // Barcode table
     $table_barcodes = $wpdb->prefix . 'barcodes';
-    $sql2 = "CREATE TABLE $table_barcodes (
+    $sql_barcodes = "CREATE TABLE $table_barcodes (
         id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
         barcode VARCHAR(30) NOT NULL UNIQUE,
         batch_id VARCHAR(20) NOT NULL,
@@ -37,6 +47,11 @@ function batch_id_create_tables() {
 
     // Execute SQL queries with dbDelta
     require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-    dbDelta($sql1);
-    dbDelta($sql2);
+    dbDelta($sql_batch_types);
+    dbDelta($sql_batch_ids);
+    dbDelta($sql_barcodes);
+
+    // Insert default types
+    $wpdb->insert($table_batch_types, ['id' => 1, 'name' => 'Prepayed', 'description' => 'Prepaid Batch', 'prefix' => 1]);
+    $wpdb->insert($table_batch_types, ['id' => 2, 'name' => 'Float', 'description' => 'Floating Batch', 'prefix' => 2]);
 }
