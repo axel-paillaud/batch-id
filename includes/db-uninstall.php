35f09799352f1c93aa01d766148d8eb97b3e183e
axel-paillaud <contact@axelweb.fr>
2025-03-19 11:12:35 +0100

    also drop batch_types table on uninstall

--- includes/db-uninstall.php
+++ includes/db-uninstall.php
@@ -9,4 +9,5 @@ function batch_id_remove_tables() {
     global $wpdb;
     $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}barcodes;");
     $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}batch_ids;");
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}batch_types;");
 }
